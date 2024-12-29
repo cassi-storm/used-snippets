f26d0cd98c9b09a694021fb25e859bac3e108465
cassi-storm <108718777+cassi-storm@users.noreply.github.com>
2024-12-29 15:27:44 +0530

    Update pow.cpp

--- pow.cpp
+++ pow.cpp
@@ -1,11 +1,11 @@
 const int mod_ = 1e9 + 7;
 
 int pw(int a, int x = mod_ - 2) {
-	int res = 1;
-	for (; x; x >>= 1, a = 1ll * a * a % mod_) {
+    int res = 1;
+    for (; x; x >>= 1, a = 1ll * a * a % mod_) {
         if (x & 1) {
             res = 1ll * res * a % mod_;
         }
     }
-	return res;
+    return res;
 }
